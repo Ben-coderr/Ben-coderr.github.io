698dce9cc55d4670de9df47591c09bde794b30f4
Ben-coderr <70851110+Ben-coderr@users.noreply.github.com>
2022-01-30 16:36:27 +0100

    Dark-theme

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+// Button scroll 
 let up = document.querySelector(".up");
-
 window.onscroll = function () {
     if (this.scrollY >= 700) {
         up.classList.add("show");
@@ -7,10 +7,33 @@ window.onscroll = function () {
         up.classList.remove("show");
     }
 };  
-
 up.onclick = function() {
     window.scrollTo({
         top:0,
         behavior:"smooth",
     })
 }
+
+// dark theme
+
+let dark = document.querySelector(".fa-moon");
+let logo = document.querySelector(".logo")
+let logof = document.querySelector(".logof")
+let navi = document.getElementById("nav-icon")
+
+dark.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")){
+        dark.classList.add("fa-sun");
+        dark.classList.remove("fa-moon");
+        logo.src = "images/logo_white.png";
+        logof.src = "images/logo_white.png";
+        navi.style.color = "white";
+    }else {
+        dark.classList.remove("fa-sun");
+        dark.classList.add("fa-moon");
+        logo.src = "images/logo_black.png";
+        logof.src = "images/logo_black.png";
+        navi.style.color = "black";
+    }
+};
